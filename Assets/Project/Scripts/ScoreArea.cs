fe86f548f81ee72a1d55a0d47dc8ed7f0a1f8160
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-12 01:09:51 +0530

    add particle effect for making a score

---
+++ Assets/Project/Scripts/ScoreArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+    public GameObject effectObject;
+
+    void Start()
+    {
+        effectObject.SetActive(false);
+    }
+    
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.GetComponent<Ball>() != null)
+        {
+            effectObject.SetActive(true);
+        }
+    }
+}
