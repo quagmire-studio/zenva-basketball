2f1297778c0ef82e9e95d01452c416e5e0348417
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-12 00:18:12 +0530

    add ball holding script

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject playerCamera;
+
+    public float ballDistance = 2.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+    }
+}
